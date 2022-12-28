3c189b6a03cc6cd33bfc57a47d04c2b43a401393
Daniel Hernandez <daniel.hernandez@finvero.com>
2022-12-27 18:09:01 -0600

    modified dinamic url api 4

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -45,7 +45,8 @@ if (codeTN) {
       grant_type: "authorization_code",
       code: codeTN,
     }),
-    headers: { "Content-type": "application/json", "mode": "no-cors" },
+    headers: { "Content-type": "application/json" },
+    mode: "no-cors"
   })
     .then((res) => res.json())
     .then((res) => {
